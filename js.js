f988cb23d4dec687fd4ff0a661313e63cddec4f2
Abagnale24 <sarosarioromeo.24@gmail.com>
2025-02-05 09:22:37 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+// You can add form submission logic or interactive features as needed
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you shortly.');
+});
